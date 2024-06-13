172aa27263863ce83d95c271242e0b76e350a144
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-06-13 15:47:08 +0100

    feat: file structure complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="">
+      <div>
+        <h1>Hey!</h1>
+      </div>
+    </main>
+  );
 }
