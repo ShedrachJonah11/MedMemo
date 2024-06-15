96aeb7796541714cb904697180ebb08d3e17f58c
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-06-15 22:05:25 +0100

    feat: add function file

---
+++ app/application/config.ts
@@ -0,0 +1,1 @@
+export const APP_URL = "https://api.vetmemos.com";
---
+++ app/application/utils/function.ts
@@ -0,0 +1,5 @@
+import axios, { AxiosResponse, AxiosRequestConfig } from "axios";
+// import AxiosInstance from "../api/axiosInstance";
+import { APP_URL } from "../config";
+import { toast } from "react-toastify";
+
