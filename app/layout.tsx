bf025bc69fb08c53aaa243a52b8509c1f81eb336
Desmond-Fon <desmondfon@yahoo.com>
2024-06-18 05:23:08 +0100

    worked con home nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers/appProvider";
 import { Suspense } from "react";
+import HomeNav from './components/HomeNav'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,7 +25,7 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         <Providers>
-         
+<HomeNav />         
                   <Suspense>{children}</Suspense>
                
         </Providers>
